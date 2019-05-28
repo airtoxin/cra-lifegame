a761273d9e4035b4cc93bff8b934028fef19eee1
airtoxin <airtoxin@icloud.com>
2019-05-28 09:35:04 +0900

    Use pixi

--- src/GameOfLifeCanvas.tsx
+++ src/GameOfLifeCanvas.tsx
@@ -1,5 +1,7 @@
 import React, {useEffect, useMemo, useRef} from "react";
 import { GameOfLifeState } from "./structures/GameOfLifeState";
+import { Stage, Sprite } from "react-pixi-fiber";
+import * as PIXI from "pixi.js";
 
 export interface Props {
   cellSize: number;
@@ -10,29 +12,12 @@ export const GameOfLifeCanvas: React.FunctionComponent<Props> = ({
   cellSize,
   state: { xs, ys, values }
 }) => {
-  const canvasRef = useRef<HTMLCanvasElement>(null);
-  useEffect(() => {
-    const canvas = canvasRef.current;
-    if (canvas) {
-      const ctx = canvas.getContext("2d");
-      if (ctx) {
-        ctx.clearRect(
-          0,
-          0,
-          cellSize * xs,
-          cellSize * ys
-        );
-
-        ctx.strokeStyle = "blue";
-
-        [...Array(ys)].forEach((_, y) => {
-          [...Array(xs)].forEach((_, x) => {
-            ctx.strokeRect(x * cellSize, y * cellSize, cellSize, cellSize);
-          });
-        });
-      }
-    }
-  }, [xs, ys, cellSize]);
-
-  return <canvas ref={canvasRef} width={500} height={500} style={{ backgroundColor: "white" }} />;
+  return (
+    <Stage
+      width={500}
+      height={500}
+    >
+      <Sprite texture={PIXI.Texture.from("https://i.imgur.com/IaUrttj.png")}/>
+    </Stage>
+  );
 };
